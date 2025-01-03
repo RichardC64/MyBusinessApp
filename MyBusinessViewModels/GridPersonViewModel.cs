fc13f4a2c705aa5a2f4097a6cba4959a9ded8152
rclark <rclark@c2i.fr>
2025-01-03 19:00:08 +0100

    partial properties

--- MyBusinessViewModels/GridPersonViewModel.cs
+++ MyBusinessViewModels/GridPersonViewModel.cs
@@ -1,6 +1,32 @@
-﻿namespace MyBusinessViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-public class GridPersonViewModel
+namespace MyBusinessViewModels;
+
+public partial class GridPersonViewModel : ObservableObject
 {
+    [ObservableProperty] public partial string? Name
+    {
+        get;
+        set;
+    }
+   
 
+    [ObservableProperty]
+    public partial string? LastName
+    {
+        get;
+        set;
+    }
+
+    partial void OnLastNameChanged(string? value)
+    {
+        //....
+    }
+
+    [ObservableProperty]
+    public partial int Age
+    {
+        get;
+        set;
+    }
 }
--- MyBusinessViewModelsTester/GridPersonTests.cs
+++ MyBusinessViewModelsTester/GridPersonTests.cs
@@ -1,10 +1,16 @@
-﻿namespace MyBusinessViewModelsTester;
+﻿using MyBusinessViewModels;
+
+namespace MyBusinessViewModelsTester;
 
 public class GridPersonTests
 {
     [Fact]
     public void Test1()
     {
+        var gridP = new GridPersonViewModel();
 
+        gridP.Name = "John";
+
+        gridP.LastName = "Doe";
     }
 }
