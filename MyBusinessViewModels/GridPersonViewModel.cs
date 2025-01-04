97bd97afb7373aeab813df6b871a48719349c307
Richard <rclark@c2i.fr>
2025-01-04 11:26:58 +0100

    refactoring

--- MyBusinessViewModels/GridPersonViewModel.cs
+++ MyBusinessViewModels/GridPersonViewModel.cs
@@ -4,29 +4,15 @@ namespace MyBusinessViewModels;
 
 public partial class GridPersonViewModel : ObservableObject
 {
-    [ObservableProperty] public partial string? Name
-    {
-        get;
-        set;
-    }
-   
+    [ObservableProperty] public partial string? Name { get; set; }
 
-    [ObservableProperty]
-    public partial string? LastName
-    {
-        get;
-        set;
-    }
+
+    [ObservableProperty] public partial string? LastName { get; set; }
 
     partial void OnLastNameChanged(string? value)
     {
         //....
     }
 
-    [ObservableProperty]
-    public partial int Age
-    {
-        get;
-        set;
-    }
+    [ObservableProperty] public partial int Age { get; set; }
 }
