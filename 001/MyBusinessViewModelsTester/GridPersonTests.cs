4fac8685fc462aa4a35a9e56ee8d4fe82624bac6
rclark <rclark@c2i.fr>
2025-01-05 13:45:21 +0100

    deplacements

---
+++ 001/MyBusinessViewModelsTester/GridPersonTests.cs
@@ -0,0 +1,46 @@
+﻿using MyBusinessViewModels;
+
+namespace MyBusinessViewModelsTester;
+
+public class GridPersonTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var gridPerson = new GridPersonViewModel();
+
+        gridPerson.FirstName = "John";
+        gridPerson.LastName = "Doe";
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var person = new Person
+        {
+            FirstName = "Richard",
+            LastName = "Clark",
+            Age = 60
+        };
+
+        var personVm = new PersonViewModel(person);
+
+        Assert.Equal(person.FirstName, personVm.FirstName);
+        Assert.Equal(person.LastName, personVm.LastName);
+        Assert.Equal(person.Age, personVm.Age);
+
+        personVm.FirstName = "John";
+        personVm.LastName = "Doe";
+        personVm.Age = 30;
+
+        Assert.Equal("John", personVm.Person.FirstName);
+        Assert.Equal("Doe", personVm.Person.LastName);
+        Assert.Equal(30, personVm.Person.Age);
+
+        personVm.Age = -1;
+        Assert.Equal(0, personVm.Age);
+
+        personVm.Age = 200;
+        Assert.Equal(150, personVm.Age);
+    }
+}
---
+++ 001/MyBusinessViewModelsTester/MyBusinessViewModels/PersonViewModel.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MyBusinessViewModels;
+
+public class PersonViewModel(Person person) : ObservableObject
+{
+    public Person Person => person;
+
+    // SetProperty avec un stateless callback
+    public string? FirstName
+    {
+        get => person.FirstName;
+        set => SetProperty(person.FirstName, value, person, (model, name) => model.FirstName = name);
+    }
+
+    // SetProperty avec un callback
+    public string? LastName
+    {
+        get => person.LastName;
+        set => SetProperty(person.LastName, value, lastName => person.LastName = lastName);
+    }
+
+    // SetProperty avec un stateless callback et une validation
+    public int Age
+    {
+        get => person.Age;
+        set => SetProperty(person.Age,value, person, (model, age) =>
+        {
+            if (age < 0) age = 0;
+            if (age > 150) age = 150;
+            model.Age = age;
+        });
+    }
+}
