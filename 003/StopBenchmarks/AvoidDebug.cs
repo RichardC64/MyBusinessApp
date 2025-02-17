2f681a2390b801b4649c36d8a426dbb5ffce40ea
Richard <rclark@c2i.fr>
2025-02-17 10:28:31 +0100

    Ajout StopUsingBenchmarks

---
+++ 003/StopBenchmarks/AvoidDebug.cs
@@ -0,0 +1,24 @@
+﻿internal class AvoidDebug
+{
+    private List<decimal> _priceCollection = new List<decimal> { 19.99m, 34.55m };
+
+    public decimal RunIEnumerable()
+    {
+        IEnumerable<decimal> priceCollection = _priceCollection;
+        var sum = 0m;
+        foreach (var price in priceCollection)
+            sum += price;
+
+        return sum;
+    }
+    public decimal RunIEnumerator()
+    {
+        IEnumerator<decimal> priceEnumerator = _priceCollection.GetEnumerator();
+        var sum = 0m;
+        while (priceEnumerator.MoveNext())
+
+            sum += priceEnumerator.Current;
+
+        return sum;
+    }
+}
---
+++ 003/StopBenchmarks/UsingDebug.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+internal class UsingDebug
+{
+    private List<decimal> _priceCollection = new List<decimal> { 19.99m, 34.55m };
+    public void RunIEnumerable()
+    {
+        IEnumerable<decimal> priceCollection = _priceCollection;
+        foreach (var price in priceCollection)
+        {
+            Debug.WriteLine(price);
+        }
+    }
+
+    public void RunIEnumerator()
+    {
+        IEnumerator<decimal> priceEnumerator = _priceCollection.GetEnumerator();
+        while (priceEnumerator.MoveNext())
+            Debug.WriteLine(priceEnumerator.Current);
+    }
+}
